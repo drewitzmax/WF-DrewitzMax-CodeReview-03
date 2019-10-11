b9a9d3c60e6637c9c2de893f63c37ce65f0abf74
drewitzmax <drewitz.max@googlemail.com>
2019-10-11 14:33:01 +0200

    final funtional product

--- js/script.js
+++ js/script.js
@@ -1,57 +1,76 @@
 var movieDB = [];
 
-function printMovieDetails(id){
+function printMovieDetails(id) {
     let movies = document.getElementById("movieDetails");
     let movie;
-    for(let element of movieDB ){
-        if(element.id == id){
+    for (let element of movieDB) {
+        if (element.id == id) {
             movie = element;
         }
     }
-    movies.innerHTML="";
-    printMovie(movie,movies);
+    movies.innerHTML = "";
+    printMovie(movie, movies);
 }
 
 function fillMovieList(movieList) {
     let movies = document.getElementById("movies");
-    movies.innerHTML="";
+    movies.innerHTML = "";
     for (let movie of movieList) {
         printMovie(movie, movies);
     }
 
 }
 
-function startPage(){
+function startPage() {
     getMovieDB();
-    fillMovieList(sortMovies());
-    document.getElementById("sort").addEventListener("change",function(){
+    fillMovieList(sortMovies(movieDB));
+    document.getElementById("sort").addEventListener("change", function () {
         fillMovieList(sortMovies());
     })
 }
 
 window.onload = startPage();
 window.onunload = saveDB();
-document.getElementById("movieDetails").addEventListener("click", function(){
+document.getElementById("movieDetails").addEventListener("click", function () {
     document.getElementById("movieDetails").style.display = "none";
 })
-document.getElementById("add").addEventListener("click", function(){
+document.getElementById("add").addEventListener("click", function () {
     document.getElementById("newMovie").style.display = "block";
+    document.getElementById("newMovie").addEventListener("click", function (){
+        document.getElementById("newMovie").style.display = "none";
+    })
+    document.getElementById("newMovieForm").addEventListener("click", e => e.stopPropagation())
 })
-document.getElementById("insertButton").addEventListener("click", function(){
+document.getElementById("insertButton").addEventListener("click", function () {
     addMovie();
     document.getElementById("newMovie").style.display = "none"
 })
+document.getElementById("genreSelect").addEventListener("change", function () {
+    let genre = document.getElementById("genreSelect").value;
+    let genreDiv = document.createElement("span");
+    genreDiv.textContent = genre;
+    genreDiv.id = genre;
+    genreDiv.className = "genreDiv";
+    genreDiv.addEventListener("click", function () {
+        genreDiv.parentNode.removeChild(genreDiv);
+        fillMovieList(sortMovies(getGenreMovies()));
+    })
+
+    document.getElementById("selectedGenres").appendChild(genreDiv);
+    fillMovieList(sortMovies(getGenreMovies()));
+})
 
 
-function sortMovies() {
+function sortMovies(sortDB) {
     let sort = Number(document.getElementById("sort").value);
-    let localDB = movieDB;
+    let localDB = sortDB;
 
     switch (sort) {
-        case 1:localDB.sort(function (a, b) {
-            return a.title.localeCompare(b.title)
-        });
-        break;
+        case 1:
+            localDB.sort(function (a, b) {
+                return a.title.localeCompare(b.title)
+            });
+            break;
         case 2:
             localDB.sort(function (b, a) {
                 return a.title.localeCompare(b.title)
@@ -64,18 +83,20 @@ function sortMovies() {
                 return 0;
             });
             break;
-        case 4:localDB.sort(function (b, a) {
-            if (a.id < b.id) return 1;
-            if (a.id > b.id) return -1;
-            return 0;
-        });
-        break;
-        case 5:localDB.sort(function (a, b) {
-            if (a.id < b.id) return 1;
-            if (a.id > b.id) return -1;
-            return 0;
-        });
-        break;
+        case 4:
+            localDB.sort(function (b, a) {
+                if (a.id < b.id) return 1;
+                if (a.id > b.id) return -1;
+                return 0;
+            });
+            break;
+        case 5:
+            localDB.sort(function (a, b) {
+                if (a.id < b.id) return 1;
+                if (a.id > b.id) return -1;
+                return 0;
+            });
+            break;
     }
     return localDB;
 }
@@ -86,7 +107,7 @@ function printMovie(movie, movies) {
     movieCard.className = "movieCard";
     movieCard.id = movie.id;
 
-    let addMovieDetail = function(){
+    let addMovieDetail = function () {
         printMovieDetails(movieCard.id);
     }
 
@@ -105,11 +126,13 @@ function printMovie(movie, movies) {
 
     text.appendChild(movieTitle);
     text.appendChild(movieDescription);
-    if(movies.id == "movieDetails"){
+    if (movies.id == "movieDetails") {
         let longDescription = document.createElement("p");
         longDescription.textContent = movie.longDescribtion;
         text.appendChild(longDescription);
         movies.style.display = "block";
+
+        movieCard.addEventListener("click", e => e.stopPropagation())
     }
 
     /*Like Div*/
@@ -142,12 +165,12 @@ function saveDB() {
 
 function getMovieDB() {
     let local;
-    try{
-    local = localStorage.getItem("movieDB");
-    } catch{
-    local = null;
+    try {
+        local = localStorage.getItem("movieDB");
+    } catch {
+        local = null;
     }
-    if (local == "null"||local == null) {
+    if (local == "null" || local == null) {
         movieDB = data;
     } else {
         movieDB = JSON.parse(localStorage.getItem("movieDB"));
@@ -155,26 +178,59 @@ function getMovieDB() {
     localStorage.setItem("movieDB", JSON.stringify(movieDB));
 }
 
-function addMovieToDB(){
+function addMovieToDB() {
     let title = document.getElementById("newTitle").value;
     let descibtion = document.getElementById("shortDes").value;
     let genre = document.getElementById("genre").value;
     let poster = document.getElementById("posterLink").value;
     let longDescribtion = document.getElementById("longDes").value;
 
-    let newMovie = new Movie(title,descibtion,genre,poster,longDescribtion);
+    let newMovie = new Movie(title, descibtion, genre, poster, longDescribtion);
     newMovie.id = movieDB.length;
     movieDB.push(newMovie);
+
+    document.getElementById("newTitle").value = "";
+    document.getElementById("shortDes").value = "";
+    document.getElementById("genre").value = "";
+    document.getElementById("posterLink").value = "";
+    document.getElementById("longDes").value = "";
 }
 
-function addMovie(){
+function getSelectedGenres() {
+    let genres = document.getElementsByClassName("genreDiv");
+    let genreArr = [];
+
+    for (let a of genres) {
+        genreArr.push(a.id);
+    }
+    return genreArr;
+}
+
+function getGenreMovies() {
+    let genres = getSelectedGenres();
+    let genreMovies = [];
+    if (genres.length != 0) {
+        for (let a of movieDB) {
+            for (let b of genres) {
+                if (a.genre == b) {
+                    genreMovies.push(a);
+                }
+            }
+        }
+        return genreMovies;
+    } else {
+        return movieDB;
+    }
+}
+
+function addMovie() {
     addMovieToDB();
     fillMovieList(sortMovies());
     saveDB();
 }
 
-class Movie{
-    constructor(title,descibtion,genre,poster,longDescribtion){
+class Movie {
+    constructor(title, descibtion, genre, poster, longDescribtion) {
         this.title = title;
         this.descibtion = descibtion;
         this.genre = genre;
